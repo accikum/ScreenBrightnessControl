cae0e1644ff2907602ac1bddf139e7decc7f6ea1
luchangwei <lucweirain@gmail.com>
2015-07-31 18:18:51 +0800

    update

--- app/src/main/java/com/lcw/screenbrightnesscontrol/light/LightSettingActivity.java
+++ app/src/main/java/com/lcw/screenbrightnesscontrol/light/LightSettingActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.lcw.screenbrightnesscontrol.BaseActivity;
 import com.lcw.screenbrightnesscontrol.R;
@@ -17,7 +18,9 @@ import com.lcw.screenbrightnesscontrol.R;
 /**
  * Created by luchangwei on 2015/7/30.
  */
-public class LightSettingActivity extends BaseActivity {
+public class LightSettingActivity extends BaseActivity implements View.OnClickListener {
+    private Button main_exit_btn;
+    private Button main_back_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,54 +31,58 @@ public class LightSettingActivity extends BaseActivity {
 
         initView();
         initLightMode();
+
+        intent = new Intent();
+        intent.setClass(getApplicationContext(), LightService.class);
+        startService(intent);
     }
 
     public static View view;
     public static WindowManager manager;
+    private static Intent intent;
 
     private void initLightMode() {
-        if (view == null && manager == null) {
-            manager = (WindowManager) getApplicationContext().getApplicationContext()
-                    .getSystemService(Context.WINDOW_SERVICE);
+        manager = (WindowManager) getApplicationContext().getApplicationContext()
+                .getSystemService(Context.WINDOW_SERVICE);
 
-            WindowManager.LayoutParams lp = lp = new WindowManager.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN
-                            | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-                            | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
-                            | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-                            | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
-                    PixelFormat.TRANSLUCENT
-                            | WindowManager.LayoutParams.FIRST_SYSTEM_WINDOW);
+        WindowManager.LayoutParams lp = lp = new WindowManager.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+                        | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                        | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+                        // | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                        | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
+                PixelFormat.TRANSLUCENT
+                        | WindowManager.LayoutParams.FIRST_SYSTEM_WINDOW);
 
-            view = new View(getApplicationContext());
-            view.setBackgroundColor(getResources().getColor(R.color.system_color_tran));
-            manager.addView(view, lp);
+        view = new View(getApplicationContext());
+        view.setBackgroundColor(getResources().getColor(R.color.system_color_tran));
+        manager.addView(view, lp);
+    }
 
-            final Intent intent = new Intent();
-            intent.setClass(getApplicationContext(), LightService.class);
-            startService(intent);
+    public Handler handler = new Handler();
 
-//            handler.postDelayed(new Runnable() {
-//                @Override
-//                public void run() {
-//                    if (view != null && manager != null) {
-//                        manager.removeView(view);
-//                        manager = null;
-//                        stopService(intent);
-//                    }
-//                    finish();
-//                }
-//            }, 20000);
+    private void initView() {
+        main_exit_btn = findView(R.id.main_exit_btn);
+        main_exit_btn.setOnClickListener(this);
+        main_back_btn = findView(R.id.main_back_btn);
+        main_back_btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.main_exit_btn:
+                stopService(intent);
+                finish();
+                break;
+            case R.id.main_back_btn:
+                onBackPressed();
+                break;
+            default:
+                break;
         }
     }
-    public Handler handler = new Handler();
-    private void initView() {
-
-    }
-}
-class LightMode{
-
 }
